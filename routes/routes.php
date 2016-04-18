c47927df6c5d74d6cfdd6d72959caa797e4fc5c8
Stanley Ojadovwa <stanwafi@gmail.com>
2016-04-18 16:38:53 +0100

    Fixed middleware error in Laravel 5.2

--- routes/routes.php
+++ routes/routes.php
@@ -6,12 +6,16 @@
 
 $router = app('router');
 
-$router->match(['GET', 'POST'], '/social/{provider}/authorize', [
-    'as' => 'socialplus.authorize',
-    'uses' => 'Stanwarri\SocialPlus\AuthorizeController@authorize'
-])->where('provider', 'facebook|twitter|linkedin|google');
+$router->middlewareGroup(['middleware' => ['web']], function () {
 
-$router->match(['GET', 'POST'], '/social/{provider}/callback', [
-    'as' => 'socialplus.callback',
-    'uses' => 'Stanwarri\SocialPlus\AuthorizeController@callback'
-])->where('provider', 'facebook|twitter|linkedin|google');
+	$router->match(['GET', 'POST'], '/social/{provider}/authorize', [
+	    'as' => 'socialplus.authorize',
+	    'uses' => 'Stanwarri\SocialPlus\AuthorizeController@authorize'
+	])->where('provider', 'facebook|twitter|linkedin|google');
+
+	$router->match(['GET', 'POST'], '/social/{provider}/callback', [
+	    'as' => 'socialplus.callback',
+	    'uses' => 'Stanwarri\SocialPlus\AuthorizeController@callback'
+	])->where('provider', 'facebook|twitter|linkedin|google');
+
+})
