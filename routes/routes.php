11ef908899a0f2ea0cb3aadb5cff2300e9892a0a
Stanley Ojadovwa <stanwafi@gmail.com>
2016-04-18 16:21:03 +0100

    Fixed unknown provider error

--- routes/routes.php
+++ routes/routes.php
@@ -9,9 +9,9 @@ $router = app('router');
 $router->match(['GET', 'POST'], '/social/{provider}/authorize', [
     'as' => 'socialplus.authorize',
     'uses' => 'Stanwarri\SocialPlus\AuthorizeController@authorize'
-])->where('provider', 'facebook|twitter|linkedin|googleplus');
+])->where('provider', 'facebook|twitter|linkedin|google');
 
 $router->match(['GET', 'POST'], '/social/{provider}/callback', [
     'as' => 'socialplus.callback',
     'uses' => 'Stanwarri\SocialPlus\AuthorizeController@callback'
-])->where('provider', 'facebook|twitter|linkedin|googleplus');
+])->where('provider', 'facebook|twitter|linkedin|google');
